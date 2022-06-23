80c63e89b99c9b4710487cf2ef904e19e407409e
nazif-2019331079 <69594422+nazif-2019331079@users.noreply.github.com>
2022-06-23 14:35:46 +0600

    Revert "Added New Sprite and Collision Checker"

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -4,50 +4,38 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.Image;
-import java.io.IOException;
-
-import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
 import entity.Player;
-import tile.TileManager;
 
-
-
-public class GamePanel extends JPanel implements Runnable{//this class inherits JPanel class
+public class GamePanel extends JPanel implements Runnable{//this class inherites JPanel class
 	//Will Make some screen settings here
-	final int originalTileSize = 16; // 16*16 tile
-	final int scale = 4; //will multiply size 4 times 
-	public final int tileSize=originalTileSize*scale;// 64*64 tile//screen size
+	final int originalTileSize=16;// 16*16 tile
+	final int scale=3;//will multiply size 3 times 
+	public final int tileSize=originalTileSize*scale;//48*48 tile//screen size
 	
-	public final int maxScreenCol = 22;// 22 tiles horizontally
-	public final int maxScreenRow = 12;// 12 tiles vertically
-	public final int screenWidth = tileSize*maxScreenCol;// 64*22 = 1408(-42 == 1366) Pixels
-	public final int screenHeight = tileSize*maxScreenRow;// 64*12 == 768 Pixels
+	final int maxScreenCol=16;//16 tiles horizontally
+	final int maxScreenRow=12;//12 tiles vertically
+	final int screenWidth=tileSize*maxScreenCol;//768 Pixels
+	final int screenHeight=tileSize*maxScreenRow;//576 Pixels
 	
 	//FPS
 	int FPS=60;
 	
-	// Map
-	TileManager tileH = new TileManager(this); 
-	// KeyListener
-	KeyHandler keyH= new KeyHandler();
-	// Collision Checker
-	public CollisionChecker cChecker = new CollisionChecker(this);
-	
+	KeyHandler keyH=new KeyHandler();
 	Thread gameThread;
+	Player player = new Player();
 	
-	//***
-	Player player = new Player (this,keyH) ; 
-	
-	
+	//player's default position
+	int playerX=100;
+	int playerY=100;
+	int playerSpeed=4;
 	
 	public GamePanel() {
 		this.setPreferredSize(new Dimension(screenWidth,screenHeight));//set the size of this class(JPanel
 		this.setBackground(Color.black);
 		this.setDoubleBuffered(true);//if set to true,all the drawing from this component will be done in an offscreen painting buffer 
-		//enabling this can improve game's rendering performance
+		//enabling this can improve gane's renderring performance
 		this.addKeyListener(keyH);
 		this.setFocusable(true);
 	}
@@ -57,54 +45,103 @@ public class GamePanel extends JPanel implements Runnable{//this class inherits 
 		gameThread.start();//it automatically calls run method
 		
 	}
-	
+
 	@Override
 	public void run() {//we will create a game loop,which will be the core of our game
 		
 		double drawInterval=1000000000/FPS;
+	//	double nextDrawTime=System.nanoTime()+drawInterval;
+		
 		double delta=0;
 		long lastTime=System.nanoTime();
 		long currentTime;
-
+		long timer=0;
+		int drawCount=0;
+		
+	
 		while(gameThread!=null) {
 			
-		currentTime=System.nanoTime();
-		
-		delta+=(currentTime-lastTime)/drawInterval;
-		lastTime=currentTime;
-		
-		if(delta>=1) {
-			//Update:Update information such as character position
-			update();
-
-				//Draw: draw the screen with the updated information
-			repaint();//it's the way to call paintComponent() method
-			delta--;
-			}		
+			currentTime=System.nanoTime();
+			
+			delta+=(currentTime-lastTime)/drawInterval;
+			timer+=(currentTime-lastTime);
+			lastTime=currentTime;
+			
+			if(delta>=1) {
+				update();
+				repaint();
+				delta--;
+				drawCount++;
+			}
+			if(timer>=1000000000) {
+				drawCount=0;
+				timer=0;
+			}
+			
+//			//long currentTime=System.nanoTime();
+//;			//long currentTime2=System.currentTimeMillis();
+//
+//			//Update:Update information such as character position
+//			update();
+//			
+//			//Draw: dreaw the screen with the updated information
+//			repaint();//it's the way to call paint component method
+//			
+//			
+//			try {
+//				double remainingTime=nextDrawTime-System.nanoTime();
+//				remainingTime/=1000000;
+//				
+//		 		if(remainingTime<0) {
+//					remainingTime=0;
+//				}
+//					 
+//				
+//				Thread.sleep((long) remainingTime);
+//				nextDrawTime +=drawInterval;
+//			
+//			} catch (InterruptedException e) {
+//				
+//				e.printStackTrace();
+//			}
+			
+			
+			
 		}
-	 }
+		
+	}
 	public void update() {
 		
 		player.update();
 		
+//		if(keyH.upPressed==true) {
+//			playerY-=playerSpeed;	
+//		}
+//		else if(keyH.downPressed==true) {
+//			playerY+=playerSpeed;//pixels
+//		}
+//		else if(keyH.leftPressed==true) {
+//			playerX-=playerSpeed;
+//		}
+//		else if(keyH.rightPressed==true) {
+//			playerX+=playerSpeed;
+//		}
 		
 	}
 	
-	public void paintComponent(Graphics g) { //it's a built-in method in java
+	public void paintComponent(Graphics g) {//it's a builtin method in java
 		
 		super.paintComponent(g);
 		Graphics2D g2=(Graphics2D)g;//Graphics 2D have more functions than graphics
-	
 		
-		
-		tileH.draw(g2);
-		
-		player.draw(g2); 
+		player.draw(g2);
+//		g2.setColor(Color.white);
+//		g2.fillRect(playerX, playerY, tileSize,tileSize);
 //		
 		g2.dispose();
 		
 		
- 	}
+	}
 
 	
 	
