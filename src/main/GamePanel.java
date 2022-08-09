c5e8d9391795d53027d85dbb7db0b18daf121d56
MuntasirNahid <69505101+MuntasirNahid@users.noreply.github.com>
2022-08-09 12:04:48 +0600

    Update GamePanel.java

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -87,7 +87,7 @@ public class GamePanel extends JPanel implements Runnable{//this class inherites
 	public int titleState=0;
 	public final int playState=1;
 	public final int pauseState=2;
-	public final int dialogueState=3;
+	public final int gameOverState=3;
 	
 	
 	
